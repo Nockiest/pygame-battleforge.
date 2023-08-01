c8f66236a706641cf214dceec88acf6b8135dd72
Andrew <ondralukes06@seznam.cz>
2023-08-01 18:30:05 +0200

    less jittery unit movement

--- unit.py
+++ unit.py
@@ -32,32 +32,34 @@ class Unit:
         self.selected = selected
 
     def move_in_game_field(self, click_pos):
-        top_left_x = click_pos[0] - self.size // 2
-        top_left_y = click_pos[1] - self.size // 2
-        new_top_left_x_in_window = max(0, min(top_left_x, WIDTH - self.size))
-        new_top_left_y_in_window = max(0, min(top_left_y, HEIGHT - self.size))
-
         # Calculate the distance between the new position and the starting position in both x and y directions
-        centered_new_top_left_x_in_window = new_top_left_x_in_window + self.size // 2
-        centered_new_top_left_y_in_window = new_top_left_y_in_window + self.size // 2
-        delta_x = centered_new_top_left_x_in_window - \
-            self.start_turn_position[0]
-        delta_y = centered_new_top_left_y_in_window - \
-            self.start_turn_position[1]
+        centered_click_pos = (click_pos[0] - self.size // 2, click_pos[1] - self.size // 2)
+        delta_x = centered_click_pos[0] - self.start_turn_position[0]
+        delta_y = centered_click_pos[1] - self.start_turn_position[1]
 
         # Calculate the distance from the starting position to the new position
         distance = math.sqrt(delta_x ** 2 + delta_y ** 2)
 
         # Check if the distance exceeds the limit of base_movement
         if distance > self.base_movement:
-            # The move is not allowed, so return without changing the position
-            return "Unit can't move that far"
+            # Calculate the new position based on the line connecting the two points
+            scale_factor = self.base_movement / distance
+            new_x = int(self.start_turn_position[0] + delta_x * scale_factor)
+            new_y = int(self.start_turn_position[1] + delta_y * scale_factor)
 
-        self.x = new_top_left_x_in_window
-        self.y = new_top_left_y_in_window
+            # Update the position and the rectangle
+            self.x = new_x - self.size // 2
+            self.y = new_y - self.size // 2
+        else:
+            # The movement is within the allowed range, so set the position directly
+            self.x = centered_click_pos[0]
+            self.y = centered_click_pos[1]
+
+        # Ensure that the unit stays within the game window boundaries
+        self.x = max(0, min(self.x, WIDTH - self.size))
+        self.y = max(0, min(self.y, HEIGHT - self.size))
 
         self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
-
     def draw_as_active(self, screen):
         # print(self.x, self.y, self.start_turn_position)
         outline_rect = pygame.Rect(
