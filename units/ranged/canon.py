90e4b6b451d72614bd26a46f07f3298eb9d9615d
Andrew <ondralukes06@seznam.cz>
2023-08-25 10:36:05 +0200

    canon pierce fixed 2

--- units/ranged/canon.py
+++ units/ranged/canon.py
@@ -13,61 +13,73 @@ class Canon(Ranged):
     def __init__(self, x, y, color):
         super().__init__(hp=1, attack_range=300, attack_resistance=0.05, base_actions=1, base_movement=50,
                          size=self.size, x=x, y=y, ammo=5, icon="canon.png", color=color, cost=self.cost)
+    
+    def __repr__(self):
+        return f"Musketeer(x={self.x}, y={self.y}, color={self.color!r})"
 
     def try_attack(self, click_pos, attacked_unit):
-        res = Unit.try_attack(self, click_pos, attacked_unit)
-        if res == "UNIT ATTACKS":
-            # Calculate a line extending from the center of the unit towards the click position
-            unit_center_x, unit_center_y = self.center
-            attacked_center_x, attacked_center_y = attacked_unit.center
-            dx = attacked_center_x - unit_center_x
-            dy = attacked_center_y - unit_center_y
-            
-            # Calculate the distance between the attacker and the attacked unit
-            distance = math.sqrt(dx ** 2 + dy ** 2)
-            
-            # Calculate the angle between the attacker and the attacked unit
-            angle_radians = math.atan2(dy, dx)
-            
-            # Convert the angle to degrees, ranging from 0 to 360
-            # angle_degrees = (math.degrees(angle_radians) + 360) % 360 +90
-            
-            # Calculate the coordinates of the point on the line at attack range distance
-       
-            point_x = self.center[0] + self.attack_range * math.cos(angle_radians)
-            point_y = self.center[1] + self.attack_range * math.sin(angle_radians)
-            line_points = bresenham_line(
-                self.center[0], self.center[1], point_x,    point_y
-            )
-            line_pixel_colors = get_pixel_colors(
-                line_points, background_screen)
-            prevented = self.prevent_shhooting_through_forrest(
-                line_pixel_colors, line_points)
-            if not prevented:
-                self.create_shoot_animation(line_points)
-            # Check if FORREST_GREEN is present in pixel colors
+        # print("before calling the function",game_state.living_units, attacked_unit  )
+        # # res = Unit.try_attack(self, click_pos, attacked_unit)
+        # print("after calling the function",game_state.living_units,   attacked_unit)
+        if attacked_unit not in self.enemies_in_range:
+            return "Attack not possible"
+        self.attack()
 
-            print("Attacker Center:", unit_center_x, unit_center_y)
-            print("Attacked Center:", attacked_center_x, attacked_center_y)
-            print("Distance:", distance)
-            # print("Angle in degrees:", angle_degrees)
-            print("Point at Attack Range:", point_x, point_y)
-            print("line points", line_points)
-            for unit in game_state.living_units:
-                if unit.color == self.color:
-                    continue
-                elif unit == attacked_unit:
-                    continue
-                
-                result = check_precalculated_line_square_interference(unit, line_points)
-                print(result[0], result[1], unit.rect, unit)
-                if result[0] != None:
-                    print(unit, unit.rect,  "interferes at" ,res[0], res[1])
-                    # self.try_attack( click_pos,unit)
-                    remain_hp = unit.take_damage(self)
-                    if remain_hp < 0:
-                        self.enemies_in_range.remove(unit)
-        return res
+        # Calculate a line extending from the center of the unit towards the click position
+        unit_center_x, unit_center_y = self.center
+        attacked_center_x, attacked_center_y = attacked_unit.center
+        dx = attacked_center_x - unit_center_x
+        dy = attacked_center_y - unit_center_y
+        # Calculate the distance between the attacker and the attacked unit
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+        # Calculate the angle between the attacker and the attacked unit
+        angle_radians = math.atan2(dy, dx)
+        point_x = self.center[0] + self.attack_range * math.cos(angle_radians)
+        point_y = self.center[1] + self.attack_range * math.sin(angle_radians)
+        line_points = bresenham_line(
+            self.center[0], self.center[1], int(point_x),    int(point_y)
+        )
+        line_pixel_colors = get_pixel_colors(
+            line_points, background_screen)
+        prevented = self.prevent_shhooting_through_forrest(
+            line_pixel_colors, line_points)
+        if not prevented:
+            self.create_shoot_animation(line_points)
+        # Check if FORREST_GREEN is present in pixel colors
+
+        # print("Attacker Center:", unit_center_x, unit_center_y)
+        # print("Attacked Center:", attacked_center_x, attacked_center_y)
+        # print("line points", line_points)
+        print("livng units before", game_state.living_units, attacked_unit)
+        
+        for unit in game_state.living_units.copy():
+            print("x")
+            print(unit)
+            if unit.color == self.color:
+                continue
+            # elif unit == attacked_unit:
+            #     continue
+
+            print(game_state.living_units)
+            # print(self.enemies_in_range)
+            # print(unit)
+
+            result = check_precalculated_line_square_interference(
+                unit, line_points)
+            print(result[0], result[1], unit.rect, unit)
+            if result[0] != None:
+                print(unit, unit.rect,  "interferes at", result[0], result[1])
+                # self.try_attack( click_pos,unit)
+                remain_hp = unit.take_damage(self)
+
+                if remain_hp < 0:
+                    game_state.living_units.remove(unit)
+                    self.enemies_in_range.remove(unit)
+                    print("Removed", unit, "from game_state.living_units")
+            print("x")
+            print("copy",  game_state.living_units. copy())
+
+        return "UNIT ATTACKS"
         #     if distance < self.attack_range:
         #         # Extend the line by setting the new endpoint to the attack range distance from the unit center
         #         line_length = self.attack_range
@@ -76,12 +88,12 @@ class Canon(Ranged):
         #         new_end_y = unit_center_y + line_length * math.sin(angle)
         #     print(unit_center_x, unit_center_y, new_end_x, new_end_y)
         #     line_points = bresenham_line(unit_center_x, unit_center_y, int(new_end_x), int(new_end_y))
-        #     for unit in game_state.living_units:
+        #     for unit in game_state.living_units.array:
         #         if unit.color == self.color:
         #             continue
         #         elif unit == attacked_unit:
         #             continue
-                
+
         #         res = check_precalculated_line_square_interference(unit, line_points)
         #         print(res[0], res[1], unit.rect, unit)
         #         if res[0] != None:
@@ -90,9 +102,5 @@ class Canon(Ranged):
         #             res = unit.take_damage(self)
         #             if res < 0:
         #                 self.enemies_in_range.remove(unit)
-      
-       
-         
-     
-    
+
     # zatím je pravěpodobnost zásahu jednotek v cestě 100%, musím změnit
--- units/ranged/musketeer.py
+++ units/ranged/musketeer.py
@@ -13,6 +13,9 @@ class Musketeer(Ranged):
         
         super().__init__(hp=2, attack_range=200,attack_resistance=0.05,base_actions=1, base_movement=125,
                          size=self.size, x=x, y=y, ammo=10, icon="musketeer.png",   color=color, cost=self.cost)
+        
+    def __repr__(self):
+        return f"Musketeer(x={self.x}, y={self.y}, color={self.color!r})"
 
     # Additional methods or overrides for the Musketeer class
 
